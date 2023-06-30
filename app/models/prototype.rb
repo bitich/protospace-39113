de7ed590614a680279e7a3a00d44e2c875a9f69d
bitich <bitich777@yahoo.co.jp>
2023-06-30 16:16:04 +0900

    データベースをリセット

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -3,7 +3,7 @@ class Prototype < ApplicationRecord
   validates :catch_copy, presence: true
   validates :concept, presence: true
   belongs_to :user
-  has_many :comments,dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_one_attached :image
 
 
