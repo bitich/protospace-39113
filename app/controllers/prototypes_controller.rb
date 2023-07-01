7fe00e219c84a49947f5e53deb72a67cf957fb89
bitich <bitich777@yahoo.co.jp>
2023-07-01 15:10:31 +0900

    インスタンスの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -33,11 +33,11 @@ class PrototypesController < ApplicationController
   end
 
   def update
-      prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
+      @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
        redirect_to prototype_path(prototype.id)
     else
-      render "prototypes/edit/#{prototype.id}"
+      render :edit
     end 
   end
 
